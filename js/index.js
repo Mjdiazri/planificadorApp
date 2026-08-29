318fbbd4301a32261916d4a49b8add46b68820d7
Mjdiazri <citruss2012@hotmail.es>
2026-08-29 16:27:13 -0500

    Se ajusta estilo de seccion 1 y cards en seccion 2

--- js/index.js
+++ js/index.js
@@ -35,10 +35,6 @@ const taskManager = new TaskManager();
    })
 
 
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
 //FUNCIONES
 
     //Funcion validar campos
